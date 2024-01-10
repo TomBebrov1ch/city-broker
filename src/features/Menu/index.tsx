d5cf4774cb135279a3402b6687e13e4d202a81b6
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-01-10 23:58:38 +0600

    Version 1.0.8

--- src/features/Menu/index.tsx
+++ src/features/Menu/index.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { Link } from "react-scroll";
+
+import styles from "./styles.module.scss";
 
 export const Menu = () => {
-  return <div>index</div>;
+  return (
+    <div className={styles.menu}>
+      <nav className={styles.menu__nav}>
+        <Link to="main" className={`${styles.menu__nav__link}`}>
+          Главная
+        </Link>
+        <Link to="about" className={`${styles.menu__nav__link}`}>
+          О Компании
+        </Link>
+        <Link to="services" className={`${styles.menu__nav__link}`}>
+          Услуги
+        </Link>
+        <Link to="markets" className={`${styles.menu__nav__link}`}>
+          Площадки
+        </Link>
+        <Link to="contacts" className={`${styles.menu__nav__link}`}>
+          Контакты
+        </Link>
+      </nav>
+    </div>
+  );
 };

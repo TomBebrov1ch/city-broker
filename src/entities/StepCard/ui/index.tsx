25b34695e538b6c9b0e2b344b6320647f7d461b0
Ruslan Makhmatov <ruslanmakhmatov@gmail.com>
2024-01-10 13:41:26 +0600

    Version 1.0.3

--- src/entities/StepCard/ui/index.tsx
+++ src/entities/StepCard/ui/index.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 
+import styles from "./styles.module.scss";
+
 interface StepCardData {
   number: string;
+  heading: string;
   text: string;
+  marginTop: string;
 }
 
-export const StepCard = () => {
-  return <div>index</div>;
+export const StepCard: React.FC<StepCardData> = ({
+  number,
+  text,
+  heading,
+  marginTop,
+}) => {
+  return (
+    <div className={`${styles.step_card} ${marginTop}`}>
+      <span className={styles.step_card__number}>{number}</span>
+      <span className={styles.step_card__heading}>{heading}</span>
+      <hr className={styles.step_card__separator} />
+      <p className={styles.step_card__paragraph}>{text}</p>
+    </div>
+  );
 };
--- src/widgets/AboutScreen/index.tsx
+++ src/widgets/AboutScreen/index.tsx
@@ -1,10 +1,11 @@
 import { Separator } from "@entities/Separator/ui";
+import { StepCard } from "@entities/StepCard/ui";
 
 import styles from "./styles.module.scss";
 
 export const AboutScreen = () => {
   return (
-    <section className="container m-auto mt-10">
+    <section className="container m-auto mt-16">
       <Separator text="О нас" />
       <h2 className={styles.heading}>О компании</h2>
       <p className="paragraph mt-4">
@@ -12,6 +13,27 @@ export const AboutScreen = () => {
         опытом, уверенно удерживает позицию лидера в рядах брокерских фирм на
         фондовом рынке.
       </p>
+      <span className={styles.sub_heading}>
+        Наша схема <span className="text-custom-red">работы</span> очень проста:
+      </span>
+      <StepCard
+        marginTop="mt-16"
+        number="1"
+        heading="Подача Заявки"
+        text="Обсудите с брокером детали закупки, после чего он разместит аукцион на бирже. Затем заказчик проведёт отбор поставщиков на участие в аукционе."
+      />
+      <StepCard
+        marginTop="mt-16"
+        number="2"
+        heading="Внесение Обеспечения"
+        text="Заказчик выбирает поставщиков, которые вносят гарантии, затем следуют торги и заключение сделки по оптимальной цене с биржевым договором."
+      />
+      <StepCard
+        marginTop="mt-16"
+        number="3"
+        heading="Исполнение Поставки"
+        text="Поставщик выполняет условия договора, обеспечивая отгрузку товара в соответствии с условиями, оговоренными с Заказчиком."
+      />
     </section>
   );
 };
